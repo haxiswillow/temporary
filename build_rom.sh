4fcaf38a8419c732d518c827c08e5b167276c6c3
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-29 20:54:16 +0700

    Fix Baseband Unknown

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ArkanMuhammad1986/local_manifest.git --depth 1 -b e
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch exthm_mido-userdebug
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
