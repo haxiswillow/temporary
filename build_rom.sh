3dc533af115fd87467bdbeba6d4147647593f02c
RasyidAlKautsar <93345864+ArkanMuhammad1986@users.noreply.github.com>
2021-11-30 06:53:04 +0700

    With GApps

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,9 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 . build/envsetup.sh
 lunch exthm_mido-userdebug
-export TZ=Asia/Dhaka #put before last build command
+export EXTHM_GAPPS=true
+export TARGET_GAPPS_ARCH=arm64
+export TZ=Asia/Jakarta #put before last build command
 mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
